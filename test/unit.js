2ffe980291acc0e89a1ebd71afe14a9d5eaf063e
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-07 10:00:45 +0300

    Add tests for AssemblyScript, Rust, and WAT

--- test/unit.js
+++ test/unit.js
@@ -3,17 +3,52 @@
 const metatests = require('metatests');
 const { load } = require('..');
 
-metatests.test('Load WASM with callback', async (test) => {
+const PATH = './test/examples/';
+
+metatests.test('Rust WASM: wasm-pack', async (test) => {
   const callback = (res) => {
     test.strictEqual(res, 10);
   };
 
-  const example = await load('./test/example.wasm', 'wbg', [callback]);
+  const fileName = PATH + 'rust.wasm';
+  const example = await load(fileName, 'wbg', [callback]);
 
   const sum = example.instance.exports.add(3, 7);
   test.strictEqual(sum, 10);
 
   example.instance.exports.add_callback(3, 7);
+
+  test.end();
+});
+
+metatests.test('WAT WASM: wabt/wat2wasm', async (test) => {
+  const callback = (res) => {
+    test.strictEqual(res, 10);
+  };
+
+  const fileName = PATH + 'wat.wasm';
+  const example = await load(fileName, 'imports', [callback]);
+
+  const sum = example.instance.exports.add(3, 7);
+  test.strictEqual(sum, 10);
+
+  example.instance.exports.addCallback(3, 7);
+
+  test.end();
+});
+
+metatests.test('AssemblyScript WASM', async (test) => {
+  const callback = (res) => {
+    test.strictEqual(res, 10);
+  };
+
+  const fileName = PATH + 'assemblyscript.wasm';
+  const example = await load(fileName, 'example', [callback]);
+
+  const sum = example.instance.exports.add(3, 7);
+  test.strictEqual(sum, 10);
+
+  example.instance.exports.addCallback(3, 7);
 
   test.end();
 });
