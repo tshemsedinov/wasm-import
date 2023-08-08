2ebd4506bdb2f017828d9be6ba51f120ec0fe0a4
Pavel <pavel.mikhalkevich@midge-medical.com>
2023-08-08 17:43:16 +0500

    Rm 'prepareWasmImports' from root d.ts; clean up in lib/node.js

--- loader.d.ts
+++ loader.d.ts
@@ -1,11 +1,5 @@
 export function load(
   fileName: string,
   moduleName: string,
-  imports: Array<Function>,
+  callbacks: Array<Function>,
 ): Promise<object>;
-
-export function prepareWasmImports(
-  byteCode: WebAssembly.Module,
-  moduleName: string,
-  entitiesToInject: Array<Function>,
-): object;
