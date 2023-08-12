afcc9fbe3d928eb8f9284ae78a09b57fb68f353a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-12 05:35:11 +0300

    Rwrite tests to use in-place callbacks
    Refs: https://github.com/tshemsedinov/wasm-import/issues/7

--- test/node.js
+++ test/node.js
@@ -6,65 +6,105 @@ const { load } = require('..');
 const PATH = './test/examples/';
 
 metatests.test('Rust WASM (node): wasm-pack', async (test) => {
-  const callback = (res) => {
-    test.strictEqual(res, 10);
-  };
-
   const fileName = PATH + 'rust.wasm';
-  const example = await load(fileName, 'example', [callback, callback]);
+  const example = await load(fileName);
 
   const res = example.instance.exports.sum(3, 7);
   test.strictEqual(res, 10);
 
-  example.instance.exports.add(3, 7);
+  example.instance.exports.add(1, 1, (res) => {
+    test.strictEqual(res, 2);
+  });
+
+  example.instance.exports.add(2, 2, (res) => {
+    test.strictEqual(res, 4);
+  });
+
+  example.instance.exports.sub(10, 5, (res) => {
+    test.strictEqual(res, 5);
+  });
+
+  example.instance.exports.sub(20, 10, (res) => {
+    test.strictEqual(res, 10);
+  });
 
   test.end();
 });
 
 metatests.test('WAT WASM (node): wabt/wat2wasm', async (test) => {
-  const callback = (res) => {
-    test.strictEqual(res, 10);
-  };
-
   const fileName = PATH + 'wat.wasm';
-  const example = await load(fileName, 'example', [callback, callback]);
+  const example = await load(fileName);
 
   const res = example.instance.exports.sum(3, 7);
   test.strictEqual(res, 10);
 
-  example.instance.exports.add(3, 7);
+  example.instance.exports.add(1, 1, (res) => {
+    test.strictEqual(res, 2);
+  });
+
+  example.instance.exports.add(2, 2, (res) => {
+    test.strictEqual(res, 4);
+  });
+
+  example.instance.exports.sub(10, 5, (res) => {
+    test.strictEqual(res, 5);
+  });
+
+  example.instance.exports.sub(20, 10, (res) => {
+    test.strictEqual(res, 10);
+  });
 
   test.end();
 });
 
 metatests.test('AssemblyScript WASM (node)', async (test) => {
-  const callback = (res) => {
-    test.strictEqual(res, 10);
-  };
-
   const fileName = PATH + 'as.wasm';
-  const example = await load(fileName, 'example', [callback, callback]);
+  const example = await load(fileName);
 
   const res = example.instance.exports.sum(3, 7);
   test.strictEqual(res, 10);
 
-  example.instance.exports.add(3, 7);
+  example.instance.exports.add(1, 1, (res) => {
+    test.strictEqual(res, 2);
+  });
+
+  example.instance.exports.add(2, 2, (res) => {
+    test.strictEqual(res, 4);
+  });
+
+  example.instance.exports.sub(10, 5, (res) => {
+    test.strictEqual(res, 5);
+  });
+
+  example.instance.exports.sub(20, 10, (res) => {
+    test.strictEqual(res, 10);
+  });
 
   test.end();
 });
 
 metatests.test('C++ WASM (node)', async (test) => {
-  const callback = (res) => {
-    test.strictEqual(res, 10);
-  };
-
   const fileName = PATH + 'cpp.wasm';
-  const example = await load(fileName, 'env', [callback, callback]);
+  const example = await load(fileName);
 
   const res = example.instance.exports.sum(3, 7);
   test.strictEqual(res, 10);
 
-  example.instance.exports.add(3, 7);
+  example.instance.exports.add(1, 1, (res) => {
+    test.strictEqual(res, 2);
+  });
+
+  example.instance.exports.add(2, 2, (res) => {
+    test.strictEqual(res, 4);
+  });
+
+  example.instance.exports.sub(10, 5, (res) => {
+    test.strictEqual(res, 5);
+  });
+
+  example.instance.exports.sub(20, 10, (res) => {
+    test.strictEqual(res, 10);
+  });
 
   test.end();
 });
