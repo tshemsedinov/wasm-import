c912333a1d72e5a8817c413523a6a1ed39b8db09
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-12 05:34:01 +0300

    Implement in-place callbacks
    Closes: https://github.com/tshemsedinov/wasm-import/issues/7
    PR-URL: https://github.com/tshemsedinov/wasm-import/pull/11

--- dist/loader.mjs
+++ dist/loader.mjs
@@ -1,19 +1,50 @@
-const prepareWasmImports = (byteCode, moduleName, callbacksToInject) => {
-  const wasmExpectedImports = WebAssembly.Module.imports(byteCode);
-  const imports = {};
-  for (const [i, { name }] of wasmExpectedImports.entries()) {
-    imports[name] = callbacksToInject[i];
+const CALLBACK_LEN = 'Callback'.length;
+const calls = new Map();
+
+const prepareImports = (byteCode) => {
+  const imports = WebAssembly.Module.imports(byteCode);
+  const expected = {};
+  for (const entry of imports) {
+    if (entry.kind !== 'function') continue;
+    let module = expected[entry.module];
+    if (!module) module = expected[entry.module] = {};
+    module[entry.name] = (...args) => {
+      const name = entry.name.slice(0, -CALLBACK_LEN);
+      const callbacks = calls.get(name);
+      if (!callbacks) return;
+      const callback = callbacks.shift();
+      if (!callback) return;
+      callback(...args);
+    };
   }
-  return { [moduleName]: imports };
+  return expected;
 };
 
-const load = async (fileName, moduleName, callbacks) => {
+const load = async (fileName, importObject = {}) => {
   const response = await fetch(fileName);
-  const wasmBuffer = await response.arrayBuffer();
-  const wasmCompiled = await WebAssembly.compile(wasmBuffer);
-  const wasmImports = prepareWasmImports(wasmCompiled, moduleName, callbacks);
-  const instance = await WebAssembly.instantiate(wasmCompiled, wasmImports);
-  return { instance, module: wasmCompiled };
+  const buffer = await response.arrayBuffer();
+  const compiled = await WebAssembly.compile(buffer);
+  const expected = prepareImports(compiled);
+  const imports = { ...importObject, ...expected };
+  const instance = await WebAssembly.instantiate(compiled, imports);
+  const exports = {};
+  for (const [name, fn] of Object.entries(instance.exports)) {
+    if (typeof fn !== 'function') {
+      exports[name] = fn;
+      continue;
+    }
+    exports[name] = (...args) => {
+      if (typeof args.at(-1) !== 'function') return fn(...args);
+      let callbacks = calls.get(name);
+      if (!callbacks) {
+        callbacks = [];
+        calls.set(name, callbacks);
+      }
+      callbacks.push(args.pop());
+      return fn(...args);
+    };
+  }
+  return { instance: { exports }, module: compiled };
 };
 
 export { load };
