26c0118553806072e96ad58be9d51f1f554a7557
Pavel <pavel.mikhalkevich@midge-medical.com>
2023-08-13 13:45:42 +0500

    Update dist/loader accordingly

--- dist/loader.mjs
+++ dist/loader.mjs
@@ -1,6 +1,8 @@
 const CALLBACK_LEN = 'Callback'.length;
 const calls = new Map();
 
+const fnNameFromCallbackName = (cbName) => cbName.slice(0, -CALLBACK_LEN);
+
 const prepareImports = (byteCode) => {
   const imports = WebAssembly.Module.imports(byteCode);
   const expected = {};
@@ -8,16 +10,46 @@ const prepareImports = (byteCode) => {
     if (entry.kind !== 'function') continue;
     let module = expected[entry.module];
     if (!module) module = expected[entry.module] = {};
+
+    const exportedFnName = fnNameFromCallbackName(entry.name);
+    calls.set(exportedFnName, []);
+
     module[entry.name] = (...args) => {
-      const name = entry.name.slice(0, -CALLBACK_LEN);
-      const callbacks = calls.get(name);
-      if (!callbacks) return;
+      const callbacks = calls.get(exportedFnName);
+      if (!callbacks) return null;
       const callback = callbacks.shift();
-      if (!callback) return;
-      callback(...args);
+      return callback ? callback(...args) : null;
     };
   }
   return expected;
+};
+
+const prepareExports = (instance) => {
+  const exports = {};
+
+  for (const [name, fn] of Object.entries(instance.exports)) {
+    if (typeof fn !== 'function') {
+      exports[name] = fn;
+      continue;
+    }
+    exports[name] = (...args) => {
+      const callbacksExpected = calls.get(name);
+      if (!callbacksExpected) return fn(...args);
+
+      const callbackProvided = typeof args.at(-1) === 'function';
+      if (callbackProvided) {
+        callbacksExpected.push(args.pop());
+        return fn(...args);
+      }
+
+      return new Promise((resolve) => {
+        callbacksExpected.push(resolve);
+        fn(...args);
+      });
+    };
+  }
+
+  return exports;
 };
 
 const load = async (fileName, importObject = {}) => {
@@ -27,23 +59,7 @@ const load = async (fileName, importObject = {}) => {
   const expected = prepareImports(compiled);
   const imports = { ...importObject, ...expected };
   const instance = await WebAssembly.instantiate(compiled, imports);
-  const exports = {};
-  for (const [name, fn] of Object.entries(instance.exports)) {
-    if (typeof fn !== 'function') {
-      exports[name] = fn;
-      continue;
-    }
-    exports[name] = (...args) => {
-      if (typeof args.at(-1) !== 'function') return fn(...args);
-      let callbacks = calls.get(name);
-      if (!callbacks) {
-        callbacks = [];
-        calls.set(name, callbacks);
-      }
-      callbacks.push(args.pop());
-      return fn(...args);
-    };
-  }
+  const exports = prepareExports(instance);
   return { instance: { exports }, module: compiled };
 };
 
