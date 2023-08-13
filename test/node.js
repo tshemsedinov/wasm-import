9ec2fde3b2487be7c9bc28e916ef0636acfedf9c
Pavel <pavel.mikhalkevich@midge-medical.com>
2023-08-13 13:39:03 +0500

    Pass ref to 'resolve' as an expected callback

--- test/node.js
+++ test/node.js
@@ -35,24 +35,32 @@ metatests.test('WAT WASM (node): wabt/wat2wasm', async (test) => {
   const fileName = PATH + 'wat.wasm';
   const example = await load(fileName);
 
-  const res = example.instance.exports.sum(3, 7);
+  let res = example.instance.exports.sum(3, 7);
   test.strictEqual(res, 10);
 
   example.instance.exports.add(1, 1, (res) => {
     test.strictEqual(res, 2);
   });
+  res = await example.instance.exports.add(1, 1);
+  test.strictEqual(res, 2);
 
   example.instance.exports.add(2, 2, (res) => {
     test.strictEqual(res, 4);
   });
+  res = await example.instance.exports.add(2, 2);
+  test.strictEqual(res, 4);
 
   example.instance.exports.sub(10, 5, (res) => {
     test.strictEqual(res, 5);
   });
+  res = await example.instance.exports.sub(10, 5);
+  test.strictEqual(res, 5);
 
   example.instance.exports.sub(20, 10, (res) => {
     test.strictEqual(res, 10);
   });
+  res = await example.instance.exports.sub(20, 10);
+  test.strictEqual(res, 10);
 
   test.end();
 });
