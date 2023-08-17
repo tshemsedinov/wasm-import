10c11126deec250d665084c503020ba453e36ae6
Pavel <pavel.mikhalkevich@midge-medical.com>
2023-08-17 11:17:18 +0500

    Use 2-space-indet in .zig module

--- test/examples/Zig/example.zig
+++ test/examples/Zig/example.zig
@@ -2,13 +2,13 @@ extern fn addCallback(i32) void;
 extern fn subCallback(i32) void;
 
 export fn sum(a: i32, b: i32) i32 {
-    return a + b;
+  return a + b;
 }
 
 export fn add(a: i32, b: i32) void {
-    addCallback(a + b);
+  addCallback(a + b);
 }
 
 export fn sub(a: i32, b: i32) void {
-    subCallback(a - b);
+  subCallback(a - b);
 }
